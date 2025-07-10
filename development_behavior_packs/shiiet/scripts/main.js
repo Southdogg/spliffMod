e9b3c339929a516014d05a9238917dc51a3efdf9
Southdogg <swetoliver@gmail.com>
2025-07-10 14:12:04 +0300

    drop and harvest

--- development_behavior_packs/shiiet/scripts/main.js
+++ development_behavior_packs/shiiet/scripts/main.js
@@ -1,4 +1,4 @@
-import { world, system } from "@minecraft/server";
+import { world, system, EquipmentSlot, GameMode, BlockType, EnchantmentSlot, Dimension, ItemStack } from "@minecraft/server";
 
 /** @type {import("@minecraft/server").BlockCustomComponent} */
 const BlockGrowableComponent = {
@@ -9,9 +9,31 @@ const BlockGrowableComponent = {
         if (Math.random() > growthChance) return;
 
         const growth = block.permutation.getState(growthState);
+        if(growth != 3){
         block.setPermutation(block.permutation.withState(growthState, growth + 1));
+        }
+    },
+    onPlayerInteract({block, player, dimension}, {params}){
+        const growthState = params.growth_state;
+        const growth = block.permutation.getState(growthState);
+
+        if ( growth == 3) {
+            const equippable = player.getComponent("minecraft:equippable");
+            const mainhand = equippable.getEquipmentSlot(EquipmentSlot.Mainhand);
+            const harwest = new ItemStack("demo:wildbud", 1);
+            const location = block.location;
+            if (mainhand.typeId == "minecraft:shears")
+            {
+                block.setType("minecraft:air");
+                world.sendMessage("Harvest");
+                dimension.spawnItem(harwest, { x: location.x, y: location.y, z: location.z});
+            }
+
+            
+        }
     }
 }
+
 system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
     blockComponentRegistry.registerCustomComponent("demo:growable", BlockGrowableComponent);
 });
